fcbe4dba8a67b6d4e0452de95eb47cf44d27ae72
jmlazaro <jmlazaro@localhost.localdomain>
2023-04-25 19:52:56 -0700

    document data process

--- data/data.py
+++ data/data.py
@@ -1,9 +1,12 @@
 import os
+from glob import glob
+from matplotlib.pyplot import imread
+from matplotlib.pyplot import imsave
 
 BASE_DIR = os.getcwd()
 
 DATASETS = {
-    'gs_rainfall_daily': 'gs/GPM_3IMERGDL/'
+    'gs_rainfall_daily': 'gs/GPM_3IMERGDL'
     }
 
 def download(
@@ -42,7 +45,7 @@ def download(
     template = """wget --no-directories --no-host-directories --no-parent \
     --recursive --mirror \
     --accept {accept} \
-    -l1 {url}""" \
+    -l1 {url}/""" \
     + f""" -P {save_dir} \
     -{verbose}"""
 
@@ -69,8 +72,7 @@ def download(
 
     return save_dir
 
-
-def process_gs_rainfall_daily(force=False, n_images=-1):
+def process_gs_rainfall_daily(force=False, n_images=-1, log=100) -> str:
     """
     Perform standard processing for gs_rainfall_daily
 
@@ -80,6 +82,8 @@ def process_gs_rainfall_daily(force=False, n_images=-1):
         force processing if processed data already exists
     n_images : int
         number of images to process (used for testing), -1 indicates all
+    log : int
+        print every log images, -1 means no logging
 
     Returns
     -------
@@ -87,13 +91,37 @@ def process_gs_rainfall_daily(force=False, n_images=-1):
         path to processed data
     """
 
+    raw_dir = f"{BASE_DIR}/data/datasets/{DATASETS['gs_rainfall_daily']}/raw"
     processed_dir = f"{BASE_DIR}/data/datasets/{DATASETS['gs_rainfall_daily']}/processed"
 
     # Skip if processed data exists and not reprocessing
     if os.path.exists(processed_dir) and (not force):
-        return save_dir
+        return processed_dir
 
     # Make processed_dir if it doesn't exist
     os.makedirs(processed_dir, exist_ok=True)
 
+    # Get file paths
+    raw_files = sorted(glob(raw_dir + '/*.PNG'))
+    if n_images != -1:
+        raw_files = raw_files[:n_images]
 
+    # Cropping limits
+    image_size_raw = imread(raw_files[0]).shape
+    north_lim = 300
+    south_lim = image_size_raw[0]//2
+    east_lim = image_size_raw[1]//2
+
+    for file_n, file in enumerate(raw_files):
+
+        if (log != -1) and (file_n % log == 0):
+            print(f'Processing file number {file_n} ({file.split("/")[-1]})')
+
+        # Read, scale pixels to [0.0, 1.0], crop, and save
+        image_arr = imread(file)
+        image_arr = image_arr[north_lim : south_lim, :east_lim]
+        processed_file = processed_dir + '/' \
+                            + file.split('/')[-1][:-4] + '_processed.PNG'
+        imsave(processed_file, image_arr, vmin=0.0, vmax=1.0, cmap='gray')
+
+    return processed_dir
